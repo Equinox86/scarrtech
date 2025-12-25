41b5d8f95d1b0277c05c3056b7c24f492966a7d4
stephen <scarras86@gmail.com>
2025-12-25 14:50:56 -0500

    Update about me blurb

--- src/lib/AboutMe.svelte
+++ src/lib/AboutMe.svelte
@@ -9,9 +9,10 @@
     </div>
     <div class="hero-content">
       <h2>Hi, I'm Stephen Carrasquillo</h2>
+      <p> Thanks for visiting my project space!</p>
+      <p>I am passionate tech-enthusiast with experience in electronics, firmware, and software development.</p>
       <p>
-        A passionate tech-enthusiast with experience in electronics, firmware, and software development.
-        Building cool stuff has always been a past-time for me and this site is where I will document all of my developmental projects.
+        Building cool stuff has always been a past-time for me, and this site is where I will document all the progress projects.
       </p>
       <p class="meta">I encourage you to check out my projects page for updates on what I am currently working on. </p>
       <a class="cta" href="#contact">Get in touch</a>
